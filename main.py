58b46705c80cc737f449e453baa8015fc09b1aca
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2026-09-10 22:28:12 +0100

    Add blank line at the beginning of main.py for improved readability

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, UploadFile, File, Form, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
